0fbec63720f10e6e3de0320b667668cfe7d24e62
korsejong <sejong.400@gmail.com>
2019-04-19 11:56:17 +0900

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
 const User = require('../models/user');
 const Item = require('../models/item');
+const Order = require('../models/order');
 
 router.get('/', async (req, res) => {
-    let users = await User.findAll().populate('orderItems.item');
+    let users = await User.findAll();
     let items = await Item.findAll();
+    let orders = await Order.findAll().populate('item').populate('user');
     res.render('index', {
         users: users,
-        items: items
+        items: items,
+        orders: orders
     });
 });
 
